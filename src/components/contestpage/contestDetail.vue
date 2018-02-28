52ce02f4e791adb63883f150b07b75a753cefb92
Saurus <jia1saurus@gmail.com>
2018-03-01 00:48:29 +0800

    登陆界面与注册界面基本完成，添加加密传输组件，组件间通讯待实现

--- src/components/contestpage/contestDetail.vue
+++ src/components/contestpage/contestDetail.vue
@@ -75,19 +75,19 @@ export default {
   },
   computed:{
     startIn:function(){
-      var date3 = new Date(this.startTime).getTime() - new Date(this.nowTime).getTime();   //时间差的毫秒数       
-      //------------------------------  
-      //计算出相差天数  
-      var days=Math.floor(date3/(24*3600*1000))  
-      //计算出小时数  
-      var leave1=date3%(24*3600*1000)    //计算天数后剩余的毫秒数  
-      var hours=Math.floor(leave1/(3600*1000))  
-      //计算相差分钟数  
-      var leave2=leave1%(3600*1000)        //计算小时数后剩余的毫秒数  
-      var minutes=Math.floor(leave2/(60*1000))  
-      //计算相差秒数  
-      var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数  
-      var seconds=Math.floor(leave3/1000)  
+      var date3 = new Date(this.startTime).getTime() - new Date(this.nowTime).getTime();   //时间差的毫秒数
+      //------------------------------
+      //计算出相差天数
+      var days=Math.floor(date3/(24*3600*1000))
+      //计算出小时数
+      var leave1=date3%(24*3600*1000)    //计算天数后剩余的毫秒数
+      var hours=Math.floor(leave1/(3600*1000))
+      //计算相差分钟数
+      var leave2=leave1%(3600*1000)        //计算小时数后剩余的毫秒数
+      var minutes=Math.floor(leave2/(60*1000))
+      //计算相差秒数
+      var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
+      var seconds=Math.floor(leave3/1000)
       var el=this
       setInterval(function(){el.nowTime=new Date();}, 100);
       return{
