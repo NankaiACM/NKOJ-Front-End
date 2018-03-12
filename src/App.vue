4df430ee78444054e328d705c53c2cc8a3fe8366
rmdyh <rmdyh@hotmail.com>
2018-03-12 21:25:14 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,10 @@
 <div id="app">
   <div class="shadow" v-if="isLoginShow||isSignupShow" v-on:click="exitShow"></div>
   <header>
-    <div>
-      <div id="mainIcon"><img height="50px" src="./assets/logo.png"></div>
-      <div id="mainTitle" v-on:click="changeToHome">NKU Online Judge</div>
-      <ul class="nav navbar-nav navbar-right" id="headerRight">
-        <li v-on:click="changeToSignup" class="headerRight"><a><span class="glyphicon glyphicon-user"></span> 注册</a></li>
-        <li v-on:click="changeToLogin" class="headerRight"><a><span class="glyphicon glyphicon-log-in"></span> 登录</a></li>
-      </ul>
-    </div>
+    <head-bar @toHome='changeToHome' @logIn='changeToLogin' @signUp='changeToSignup'
+              @toProblem="changeToProblems" @toStatus="changeToStatus" @toContest="changeToContest"
+              @toRank="changeTo404" @toDiscuss="changeTo404" :nowPage=nowPage :userPage=userPage></head-bar>
   </header>
-  <aside class="hidden-xs">
-    <div><img id="userPicture" src="./assets/userPicture.jpg" v-on:click="changeToUser"></div>
-    <div id="userName">User: Saurus</div>
-    <ul class="nav nav-pills nav-stacked" id="navigation">
-      <li id="home" v-on:click="changeToHome"><span class="glyphicon glyphicon-home"></span>主页</li>
-      <li id="problems" v-on:click="changeToProblems"><span class="glyphicon glyphicon-list"></span>题目</li>
-      <li id="status" v-on:click="changeToStatus"><span class="glyphicon glyphicon-stats"></span>评测</li>
-      <li id="contest" v-on:click="changeToContest"><span class="glyphicon glyphicon-tower"></span>比赛</li>
-      <li id="ranklist" v-on:click="changeTo404"><span class="glyphicon glyphicon-signal"></span>排名</li>
-      <li id="discuss" v-on:click="changeTo404"><span class="glyphicon glyphicon-comment"></span>讨论</li>
-    </ul>
-  </aside>
   <section id="main">
     <login-page v-if="isLoginShow"></login-page>
     <signup-page v-if="isSignupShow"></signup-page>
@@ -34,48 +17,59 @@
 <script>
 import loginPage from './components/loginPage'
 import signupPage from './components/signupPage'
+import headBar from './components/headBar'
 export default {
-  components: {loginPage, signupPage},
+  components: {loginPage, signupPage,headBar},
   name: 'App',
   data: function () {
     return {
       isLoginShow: false,
-      isSignupShow: false
+      isSignupShow: false,
+      nowPage: 'Home',
+      userPage: 'None',
     }
   },
   methods: {
     changeToProblems: function () {
+      this.nowPage='Problem'
       this.$router.push({
         path: '/problems'
       })
     },
     changeToHome: function () {
+      this.nowPage='Home'
       this.$router.push({
         path: '/home'
       })
     },
     changeToStatus: function () {
+      this.nowPage='Status'
       this.$router.push({
         path: '/status'
       })
     },
     changeToContest: function () {
+      this.nowPage='Contest'
       this.$router.push({
         path: '/contest'
       })
     },
     changeToUser: function () {
+      this.nowPage='User'
       this.$router.push({
         path: '/user/Saurus'
       })
     },
     changeToLogin: function () {
+      this.userPage='Login'
       this.isLoginShow = true
     },
     changeToSignup: function () {
+      this.userPage='Signup'
       this.isSignupShow = true
     },
     changeTo404: function () {
+      this.nowPage='404'
       this.$router.push({
         path: '/notFound'
       })
@@ -83,6 +77,14 @@ export default {
     exitShow: function () {
       this.isLoginShow = false
       this.isSignupShow = false
+    }
+  },
+  watch:{
+    isLoginShow:function(oldValue,newValue){
+      if(newValue==false) this.userPage='None'
+    },
+    isSignupShow:function(oldValue,newValue){
+      if(newValue==false) this.userPage='None'
     }
   }
 }
@@ -95,10 +97,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-.headerRight:hover{
-  cursor: pointer;
 }
 #navigation .glyphicon {
   position: relative;
@@ -111,13 +109,6 @@ export default {
   margin-right: 15px;
   -moz-osx-font-smoothing: grayscale;
 }
-#headerRight{
-  margin-right: 5px;
-}
-#headerRight li a{
-  color:white;
-  background: none;
-}
 * {
   padding: 0;
   margin: 0;
@@ -126,24 +117,12 @@ export default {
 
 header {
   width: 100%;
-  height: 50px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  background-color: #16aad8;
-  color: #fff;
+  height: 60px;
+  position: fixed;
+  top: 0px;
+  z-index: 1;
 }
 
-aside {
-  width: 150px;
-  position: absolute;
-  top: 50px;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgb(55, 55, 55);
-}
 .shadow{
   position: absolute;
   left: 150px;
@@ -153,30 +132,6 @@ aside {
   opacity: 0.6;
   background: black;
   z-index: 3;
-}
-#navigation li {
-  padding-top: 12px;
-  padding-bottom: 12px;
-}
-
-#navigation {
-  margin-top: 30px;
-}
-
-#problems,
-#home,
-#status,
-#contest,
-#ranklist,
-#discuss {
-  background-size: 30px;
-  background-position: 25px;
-  background-repeat: no-repeat;
-  font-size: 15px;
-  vertical-align: middle;
-  cursor: pointer;
-  color: lightsteelblue;
-  font-weight: bold;
 }
 
 /*
@@ -200,16 +155,6 @@ aside {
 }
 */
 
-#problems:hover,
-#home:hover,
-#status:hover,
-#contest:hover,
-#ranklist:hover,
-#discuss:hover {
-  background-color: black;
-  color: white;
-}
-
 #userPicture {
   height: 110px;
   margin-left: 20px;
@@ -227,31 +172,11 @@ aside {
   margin-top: 5px;
 }
 
-#mainIcon {
-  height: 50px;
-  padding-left: 20px;
-  padding-right: 20px;
-  float: left;
-}
-
-#mainTitle {
-  color: white;
-  font-size: 30px;
-  font-family: "Adobe Arabic";
-  padding: 10px 0;
-  margin: 0;
-  float: left;
-  cursor: pointer;
-}
 
 #main {
-  position: absolute;
-  left: 150px;
-  top: 50px;
-  right: 0;
-  bottom: 0;
   overflow: auto;
   background: rgb(230, 230, 230);
+  margin-top: 60px;
 }
 
 #con {
---
+++ src/components/headBar.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="head-wrapper">
+    <div class="mainIcon"><img height="50px" src="../assets/logo_new.png" v-on:click="$emit('toHome')"></div>
+    <ul class="nav navbar-nav">
+      <li :class="{focusing:nowPage=='Home'}" v-on:click="$emit('toHome')"><span class="glyphicon glyphicon-home"></span>主页</li>
+      <li :class="{focusing:nowPage=='Problem'}" v-on:click="$emit('toProblem')"><span class="glyphicon glyphicon-list"></span>题目</li>
+      <li :class="{focusing:nowPage=='Status'}" v-on:click="$emit('toStatus')"><span class="glyphicon glyphicon-stats"></span>评测</li>
+      <li :class="{focusing:nowPage=='Contest'}" v-on:click="$emit('toContest')"><span class="glyphicon glyphicon-tower"></span>比赛</li>
+      <li :class="{focusing:nowPage=='Rank'}" v-on:click="$emit('toRank')"><span class="glyphicon glyphicon-signal"></span>排名</li>
+      <li :class="{focusing:nowPage=='Discuss'}" v-on:click="$emit('toDiscuss')"><span class="glyphicon glyphicon-comment"></span>讨论</li>
+    </ul>
+    <div class="usersBar">
+      <div class="field" :class="{'field-focus':userPage=='Signup'}" v-on:click="$emit('signUp')">
+        <a href="#"> 注册<span class="glyphicon glyphicon-user"></span></a>
+      </div>
+      <div class="field" :class="{'field-focus':userPage=='Login'}" v-on:click="$emit('logIn')">
+        <a href="#"> 登录<span class="glyphicon glyphicon-log-in"></span></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headBar',
+  props:{
+    nowPage: String,
+    userPage: String,
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+.head-wrapper{
+  display: table;
+  width: 100%;
+  box-shadow: 0 4px 8px 0 rgba(7,17,27,.2);
+  background-color: #13293d;
+}
+.mainIcon {
+  height: 60px;
+  float: left;
+  padding: 5px 10px 5px 20px;
+  cursor: pointer;
+}
+.navbar-nav{
+  height: 60px;
+  display: block;
+  line-height: 50px;
+}
+.navbar-nav li{
+  color: #e8f1f2;
+  padding: 5px 1rem;
+  font-size: 1.5rem;
+  width: 10rem;
+  height: 60px;
+  overflow: hidden;
+  cursor: pointer;
+  display: inline-block;
+  white-space:nowrap;
+  transition: background-color 0.3s ease;
+}
+.navbar-nav li.focusing{
+  background-color: #0b1722;
+  cursor: default;
+  pointer-events: none;
+}
+.navbar-nav li:hover{
+  background-color: #283c4e
+}
+.navbar-nav li span{
+  margin-right: 5px;
+}
+
+
+.usersBar{
+  float: right;
+  padding-right: 20px;
+  height: 60px;
+}
+.usersBar div.field{
+  width: 4rem;
+  height: 60px;
+  display: inline-block;
+  overflow: hidden;
+  cursor: pointer;
+  margin: 0 5px;
+}
+.usersBar div.field a{
+  line-height: 60px;
+  color: #e8f1f2;
+  position: relative;
+  top: -60px;
+  transition: top 0.4s ease;
+}
+.usersBar div.field:hover a , .usersBar div.field-focus a{
+  text-decoration: none;
+  top: 0;
+}
+.usersBar div.field a:focus{
+  text-decoration: none;
+}
+.usersBar div.field a span{
+  padding: 0 1.1rem;
+}
+</style>
