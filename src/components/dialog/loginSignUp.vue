6db1387bd06b18f180645d804797329047d89d6c
rmdyh <rmdyh@hotmail.com>
2018-03-17 19:36:27 +0800

    完善注册界面

--- src/components/dialog/loginSignUp.vue
+++ src/components/dialog/loginSignUp.vue
@@ -8,11 +8,11 @@
       <hr>
       <form>
         <div class="form-group" :class="{'hastext':loginAttribute.loginAccount!='','focus':focusing==1}">
-          <label>用户名或邮箱</label>
+          <label @click="labelClick">用户名或邮箱</label>
           <input type="text" class="form-control" v-model="loginAttribute.loginAccount" @focus="focusing=1" @blur="focusing=0">
         </div>
         <div class="form-group" :class="{'hastext':loginAttribute.loginPassword!='','focus':focusing==2}">
-          <label>密码</label>
+          <label @click="labelClick">密码</label>
           <input type="password" class="form-control" v-model="loginAttribute.loginPassword" @focus="focusing=2" @blur="focusing=0">
         </div>
         <div class="form-group message-bar">
@@ -34,38 +34,56 @@
     <form>
       <!--第一阶段，输入邮箱名-->
       <div class="form-group" :class="{'hastext':signupAttribute.signupEmail!='','focus':focusing==3}">
-        <label>邮箱</label>
-        <input type="text" class="form-control" v-model="signupAttribute.signupEmail" @focus="focusing=3" @blur="focusing=0">
+        <label @click="labelClick">邮箱</label>
+        <input type="text" class="form-control" :class="{'disabled':isEmailSending || isEmailSend}" 
+            v-model="signupAttribute.signupEmail" :disabled="isEmailSending || isEmailSend" @focus="focusing=3" @blur="focusing=0">
+      </div>
+      <div class="form-group captcha" v-if="!isEmailSend" :class="{'hastext':signupAttribute.signupCaptcha!='','focus':focusing==4}">
+        <label @click="labelClick">右图中的文字</label>
+        <input type="text" class="form-control" :class="{'disabled':isEmailSending}" v-model="signupAttribute.signupCaptcha" 
+            :disabled="isEmailSending" @focus="focusing=4" @blur="focusing=0" maxlength="6">
+        <img class="captcha" :src="captchaUrl" @click="captchaUrl='http://111.231.98.20:8000/captcha/sendmail?_t=' + Math.random()"/>
       </div>
       <!--第二阶段，输入验证码-->
-      <div class="form-group" v-if="isEmailSend" :class="{'hastext':signupAttribute.signupEmail!='','focus':focusing==3}">
-        <label>邮箱</label>
-        <input type="text" class="form-control" v-model="signupAttribute.signupEmail" @focus="focusing=3" @blur="focusing=0">
+      <div class="form-group" v-if="isEmailSend" :class="{'hastext':signupAttribute.emailCode!='','focus':focusing==1}">
+        <label @click="labelClick">邮箱验证码</label>
+        <input type="text" class="form-control" v-model="signupAttribute.emailCode" @focus="focusing=1" @blur="focusing=0">
       </div>
       <div v-if="isEmailVerify" class="afterEmailSendDiv">
         <div class="form-group">
-          <label class="col-sm-3 control-label">密码</label>
+          <label @click="labelClick" class="col-sm-3 control-label">密码</label>
           <input type="password" class="form-control" v-model="signupAttribute.signupPassword">
         </div>
         <div class="form-group">
-          <label class="col-sm-3 control-label">确认密码</label>
+          <label @click="labelClick" class="col-sm-3 control-label">确认密码</label>
           <input type="password" class="form-control" v-model="signupAttribute.signupPassword2">
         </div>
         <div class="form-group">
-          <label class="col-sm-3 control-label">用户名</label>
+          <label @click="labelClick" class="col-sm-3 control-label">用户名</label>
           <input type="text" class="form-control" v-model="signupAttribute.signupName">
         </div>
         <div class="form-group">
-          <label class="col-sm-3 control-label">邮箱验证码</label>
-          <input type="text" class="form-control" v-model="emailCode">
+          <label @click="labelClick" class="col-sm-3 control-label">邮箱验证码</label>
         </div>
       </div>
       <div class="form-group message-bar">
-          <p>{{errorMessage}}</p>
+          <p>{{statusMessage}}</p>
+          <div class="text">
+            你还可以：
+            <a @click="isEmailSend=falase,
+                       signupAttribute.signupCaptcha='',
+                       showMessageBar('.appear .message-bar', 0),
+                       captchaUrl='http://111.231.98.20:8000/captcha/sendmail?_t=' + Math.random(),
+                       focusing=0">重新填写邮箱</a>或<a>重发邮件</a>
+          <span v-if="sendColdTime!=0">(还有{{sendColdTime}}s)</span>
+          </div>
       </div>
       <div class="form-group">
-        <button v-on:click.prevent="emailSendAttempt" class="btn" v-if="!isEmailSend">
-          <span class="glyphicon glyphicon-envelope" style="positon:relative;left:-5px;"></span>发送
+        <button class="btn" v-if="isEmailSending">
+          <vue-loading type="spin" color="white" :size="{ width: '30px', height: '30px' }"></vue-loading>
+        </button>
+        <button v-on:click.prevent="emailSendAttempt" class="btn" v-else-if="!isEmailSend">
+          <span class="glyphicon glyphicon-envelope" style="positon:relative;left:-5px;"></span>确定邮箱
         </button>
         <button v-on:click.prevent="emailAttempt" class="btn" v-else-if="!isEmailVerify">验证邮箱</button>
         <button v-on:click.prevent="signupAttempt" class="btn" v-else>注册</button>
@@ -78,12 +96,13 @@
 
 <script>
 import dialogWrap from "./dialogWrap";
+import vueLoading from "vue-loading-template";
 export default {
   name: "loginSignUp",
   props: {
     status: String
   },
-  components: { dialogWrap },
+  components: { dialogWrap, vueLoading },
   data() {
     return {
       loginAttribute: {
@@ -99,14 +118,19 @@ export default {
         signupPassword2: "",
         signupName: "",
         signupEmail: "",
+        signupCaptcha: "",
         signupWords: "",
-        signupMessage: ""
+        signupMessage: "",
+        emailCode: ""
       },
+      isEmailSending: false,
       isEmailSend: false,
       isEmailVerify: false,
-      emailCode: "",
-      errorMessage: "",
-      signupHeight: 250
+      statusMessage: "",
+      signupHeight: 250,
+      captchaUrl:
+        "http://111.231.98.20:8000/captcha/sendmail?_t=" + Math.random(),
+      sendColdTime: 60,
     };
   },
   methods: {
@@ -134,7 +158,7 @@ export default {
             console.log(res.body.user);
             this.$emit("userInfo", res.body.user);
           }
-          this.showMessageBar();
+          this.showMessageBar(".appear .message-bar", 2);
         });
     },
     loginAttempt: function(event) {
@@ -156,8 +180,8 @@ export default {
         this.signupAttribute.signupPassword2
       ) {
         if (typeof this.emailMessage === undefined)
-          this.$set(this.data, "errorMessage", "两次输入的密码不一致！");
-        else this.errorMessage = "两次输入的密码不一致！";
+          this.$set(this.data, "statusMessage", "两次输入的密码不一致！");
+        else this.statusMessage = "两次输入的密码不一致！";
         return;
       }
       var sendPackge = {
@@ -179,48 +203,67 @@ export default {
         .then(res => {
           console.log(res);
           if (res.body.code === 0) {
-            this.errorMessage = "注册成功！";
+            this.statusMessage = "注册成功！";
           } else {
-            this.errorMessage = "未知错误！";
+            this.statusMessage = "未知错误！";
           }
         });
     },
     emailSendAttempt: function(event) {
       //邮箱检验
-      var input = document.querySelector(".appear input")
-      input.disabled = true;
-      input.style.backgroundColor = "#ededed";
       if (!this.CheckMail(this.signupAttribute.signupEmail)) {
-        this.errorMessage = "哎呀，邮箱格式错了，再检查一下吧！";
-        this.showMessageBar();
-        input.disabled = false;
-        input.style.backgroundColor = "#fff";
+        this.statusMessage = "哎呀，邮箱格式错了，再检查一下吧！";
+        this.showMessageBar(".appear .message-bar", 2);
       } else {
-        console.log(
-          "http://111.231.98.20:8000/api/u/verify/" +
-            this.signupAttribute.signupEmail
-        );
+        this.isEmailSending = true;
         this.$http
           .get(
             "http://111.231.98.20:8000/api/u/verify/" +
-              this.signupAttribute.signupEmail,
-            { crossDomain: true, xhrFields: { withCredentials: true } }
-          )
-          .then((res, err) => {
-            if (err) {
-              console.log(err);  0
-            } else {
-              this.signupHeight = 550;
-              if (res.body.code === 0) {
-                this.isEmailSend = true;
-                this.errorMessage = "我们已经向您的邮箱发送了邮件！";
-              } else {
-                this.errorMessage = "邮箱不正确或者已经被注册！";
-                input.disabled = false;
-                input.style.backgroundColor = "#fff";
-              }
+              this.signupAttribute.signupEmail +
+              "?captcha=" +
+              this.signupAttribute.signupCaptcha,
+            {
+              crossDomain: true,
+              xhrFields: { withCredentials: true },
+              timeout: "8000",
+              cache: true,
+              credentials: true
             }
-            this.showMessageBar();
+          )
+          .then(
+            res => {
+              var vue = this;
+              console.log(res.body);
+              var resp = res.body;
+              if (resp.code === 0) {
+                vue.isEmailSend = true;
+                vue.statusMessage = "我们已经向您的邮箱发送了邮件！";
+                vue.showMessageBar(".appear .message-bar", 4);
+              } else if (resp.code === 233) {
+                vue.statusMessage = "请刷新验证码,再试一次！";
+                vue.showMessageBar(".appear .message-bar", 2);
+              } else if (resp.code === 234) {
+                vue.statusMessage = "验证码错啦！";
+                vue.showMessageBar(".appear .message-bar", 2);
+              } else {
+                vue.statusMessage = "邮箱已经被注册或暂时无法发送邮件！";
+                vue.showMessageBar(".appear .message-bar", 2);
+              }
+              vue.captchaUrl='http://111.231.98.20:8000/captcha/sendmail?_t=' + Math.random();
+              this.isEmailSending = false;
+            },
+            res => {
+              var vue = this;
+              vue.statusMessage = "电波……无法传达……（连接失败）";
+              vue.showMessageBar(".appear .message-bar", 2);
+              this.isEmailSending = false;
+            }
+          )
+          .catch(function(response) {
+            var vue = this;
+            vue.statusMessage = "通讯间发生错误……稍后再试吧";
+            vue.showMessageBar(".appear .message-bar", 2);
+            this.isEmailSending = false;
           });
       }
     },
@@ -235,13 +278,18 @@ export default {
       var filter = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
       return filter.test(mail);
     },
-    showMessageBar() {
-      var mb = document.querySelector(".appear .message-bar");
-      if (mb.style.height == 0) {
-        Velocity(mb, { height: "2rem" }, { duration: 200 });
-        Velocity(mb, { color: "#5bc0de" }, { duration: 300 });
+    showMessageBar(classcode, high) {
+      var mb = document.querySelector(classcode);
+      if (mb.style.height != high) {
+        Velocity(mb, { height: high + "rem" }, { duration: 300 });
       }
     },
+    labelClick(event) {
+      event.target.nextElementSibling.focus();
+    },
+    circle(event) {
+      console.log(event.target);
+    }
   }
 };
 </script>
@@ -288,6 +336,25 @@ export default {
   box-shadow: none;
   border-color: #66afe9;
 }
+.dialog-field input.disabled {
+  background: #dedede;
+  cursor: default;
+}
+.dialog-field input.disabled:hover {
+  border-color: #ccc;
+}
+.dialog-field .captcha input {
+  padding-right: 150px;
+}
+.dialog-field .captcha img {
+  position: absolute;
+  top: 0.5rem;
+  right: 5px;
+  width: 140px;
+  height: 46px;
+  border-radius: 3px;
+  cursor: pointer;
+}
 .dialog-field .form-group label {
   font-weight: normal;
   position: absolute;
@@ -296,6 +363,7 @@ export default {
   font-size: 1.5rem;
   transition: all 0.3s;
   color: #2c3e50;
+  cursor: text;
 }
 .dialog-field .focus label,
 .dialog-field .hastext label {
@@ -309,9 +377,8 @@ export default {
   height: 0;
   overflow: hidden;
   font-size: 1.4rem;
-  color: white;
-  line-height: 2rem;
-  font-weight: bold;
+  color: #5bc0de;
+  line-height: 2.5rem;
 }
 
 .form-group button {
