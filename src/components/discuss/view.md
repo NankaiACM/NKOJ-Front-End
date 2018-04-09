8b347b5e63aa25fa8bc8f68b700a3cfc146f5d38
baitieyi <baitieyi@163.com>
2018-04-09 15:03:52 +0800

    添加讨论.引入了moment.js.

---
+++ src/components/discuss/view.md
@@ -0,0 +1,15 @@
+list: [
+  {
+    id: '123',//讨论的id
+    title: 'abc',
+    nodes: ['a','b'],
+    livid: '22',//回复
+    sponsor: '',//发起
+    terminator: '',//最后回复,
+    s_ttamp: '',//发起时间戳
+    t_ttamp: '',//最后回复时间戳
+    v_c: '',//查看量
+    r_c: '',//回复量
+  },
+  {}
+]
--- src/components/discussPage.vue
+++ src/components/discussPage.vue
@@ -1,23 +1,41 @@
 <template>
-<div id="discussPage" class="container-fluid">
-  <div class="item">
-
+<div id="discuss-page" class="container-fluid">
+  <div id="discuss-list" class="col-md-8">
+  <discuss-cell  v-for="(item, index) in list" :key="index" :item="item"></discuss-cell>
   </div>
 </div>
 </template>
 <script>
+import discussCell from './discuss/discussCell.vue'
 export default {
   name: 'discussPage',
   data: function() {
     return {
-
+      list: [{
+        id: '123',//讨论的id
+        title: '这是标题',
+        nodes: ['a','b'],
+        sponsor: 'cc',//发起
+        terminator: 'dd',//最后回复,
+        s_ttamp: '1523177873441',//发起时间戳
+        t_ttamp: '1523177873441',//最后回复时间戳
+        v_c: '100',//查看量
+        r_c: '100',//回复量
+      }]
     }
   },
-  methods: {}
+  methods: {},
+  components: {
+    discussCell
+  }
 }
 </script>
 <style>
-#discussPage {
-  background: #fff;
+#discuss-page {
+  background: #fdfdfd;
+}
+
+#discuss-list {
+  padding-top: 60px;
 }
 </style>
