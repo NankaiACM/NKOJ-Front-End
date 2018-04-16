a342cb0c560802210c28f36f7a92be947c85c090
Sun Yi-Ran <sunrisefox@qq.com>
2018-04-16 19:38:16 +0900

    Adjust configuration to work with express. (#13)
    * 基本配置
    
    * Fix the place of index.js

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: '/var/www/app/data/dist/index.html',
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: '/var/www/app/data/dist',
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
