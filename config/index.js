eb4dc484fb0fac57fb1b0b2965adda186d4e6ba7
Sun Yi-Ran <sunrisefox@qq.com>
2018-04-16 19:35:47 +0900

    Fix the place of index.js

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: '/var/www/app/data/dist/index.html'),
 
     // Paths
     assetsRoot: '/var/www/app/data/dist',
