875eaa609deb7fcefc0bcece20cd71b29b72f66e
baitieyi <baitieyi@163.com>
2018-12-02 00:20:02 +0800

    虚拟的排名api

--- src/components/contest/virtualApi.js
+++ src/components/contest/virtualApi.js
@@ -79,8 +79,96 @@ const contestStatus = function (http, sid, lastid, start, end) {
       })
   })
 }
+
+const contestData = function (http, cid) {
+  return new Promise(function (resolve, reject) {
+    http.get(window.noPointHost + '/api/contest/cid')
+      .then(function (res) {
+        let ret = res.body.data
+        resolve(ret)
+      }, function (err) {
+        reject(err)
+      })
+  })
+}
+
+const rankList = async function (http, cid) {
+  let during = await contestData(http, cid)
+  let [startime, endtime] = JSON.parse(during)
+  let status = await contestStatus(http, cid, 0, startime, endtime)
+  const suptime = new Date(startime).getTime()
+  const getUsers = function (list) {
+    let sets = new Set([])
+    let pack = {}
+    for (let it of list) {
+      if (!sets.has(it['user_id'])) {
+        sets.add(it['user_id'])
+        pack[it['user_id']] = []
+      }
+      it['love41'] = new Date(it['when']).getTime() - suptime
+      pack[it['user_id']].push(it)
+    }
+    for (let id in pack) {
+      pack[id].sort(function (l, r) {
+        return l['solution_id'] - r['solution_id']
+      })
+    }
+    return {
+      sets: sets,
+      pack: pack
+    }
+  }
+  let users = getUsers(status)
+  // struct:
+  /* 0: how many ac
+     1: total time
+     2...: time (NaN is not ac yet)
+  */
+  const penalty = 20 * 60 * 1000
+  const ret = []
+  for (let it of users.sets) {
+    let one = new Map()
+    let add = new Map()
+    for (let ht of users.pack[it]) {
+      const pid = ht['problem_id']
+      const when = ht['love41']
+      if (ht['status_id'].toString() === 107) {
+        if (!one.has(pid)) {
+          one.set(pid, when)
+        } else {
+          one[pid] = Math.min(one[pid], when)
+        }
+      } else {
+        if (!one.has(pid)) {
+          if (!add.has(pid)) {
+            add.set(pid, penalty)
+          } else {
+            add[pid] += penalty
+          }
+        }
+      }
+    }
+    const allac = one.size
+    let alltime = 0
+    for (let i in one) {
+      alltime += one[i]
+      if (add.has(i)) alltime += add[i]
+    }
+    ret.push({
+      uid: it,
+      nickname: users.pack[0]['nickname'],
+      allac: allac,
+      alltime: alltime,
+      one: one,
+      add: add
+    })
+  }
+  return ret
+}
+
 export {
   myStatus,
   pcStatus,
-  contestStatus
+  contestStatus,
+  rankList
 }
