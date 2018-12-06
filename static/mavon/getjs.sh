b5f7d780ccd64ac58fca8261dc666c7e872f04fc
baitieyi <baitieyi@163.com>
2018-12-06 23:30:53 +0800

    warning: this file and its dir should be ignore after install

---
+++ static/mavon/getjs.sh
@@ -0,0 +1,5 @@
+wget https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.9.0/github-markdown.min.css
+wget https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.8.3/katex.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.8.3/katex.min.css
+wget https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css
