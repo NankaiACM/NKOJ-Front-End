25d55cd095219c39084fc33b1d3db2808a6aac59
listerily <listerily@outlook.com>
2021-04-20 16:10:44 +0800

    add input box for problem rejudgement

--- src/components/admin/rejudge.vue
+++ src/components/admin/rejudge.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="rejudge" class="bginputbg">
-    <h3>重新评测</h3>
+    <h3>重新评测答案</h3>
     <div id="bginput" :class="{gray: solutionId === ''}">
       <div class="b">
         <input placeholder="solution id" class="i" v-model="solutionId">
-        <div class="r" @click="rejudge">rejudge</div>
+        <div class="r" @click="rejudgeSolution">rejudge</div>
       </div>
       <div class="c" v-if="isRes">{{resMsg}}</div>
     </div>
+    <h3>重新评测题目</h3>
+    <div id="bginput" :class="{gray: problemId === ''}">
+      <div class="b">
+        <input placeholder="problem id" class="i" v-model="problemId">
+        <div class="r" @click="rejudgeProblem">rejudge</div>
+      </div>
+    </div>
+    <div class="c" v-if="isRes">{{resMsg}}</div>
   </div>
 </template>
 <script>
@@ -16,14 +24,25 @@ export default {
   data: function () {
     return {
       solutionId: '',
+      problemId: '',
       isRes: false
     }
   },
   methods: {
-    rejudge: function () {
+    rejudgeSolution: function () {
       const vm = this
       if (vm.solutionId === '') return
       vm.$http.get(window.noPointHost + '/api/judge/rejudge/' + vm.solutionId)
+        .then(function (res) {
+          console.log(JSON.stringify(res))
+          vm.isRes = true
+          vm.resMsg = JSON.stringify(res.body)
+        })
+    },
+    rejudgeProblem: function () {
+      const vm = this
+      if (vm.problemId === '') return
+      vm.$http.get(window.noPointHost + '/api/judge/rejudge/:pid' + vm.problemId)
         .then(function (res) {
           console.log(JSON.stringify(res))
           vm.isRes = true
