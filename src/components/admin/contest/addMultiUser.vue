391f5d505f5eea2d042257372c998fda162735d8
Alexhaoge <1810064@mail.nankai.edu.cn>
2021-05-05 14:13:55 +0800

    Update addMultiUser.vue
    支持输入选手姓名了

--- src/components/admin/contest/addMultiUser.vue
+++ src/components/admin/contest/addMultiUser.vue
@@ -9,19 +9,32 @@
     <span v-if="coldTime!==0">(冷却时间{{coldTime}}s)</span>
     <span v-else>生成用户</span>
   </button>
+  <div style="height:10rem; width: 100%;">
+  <textarea placeholder="参赛选手姓名，以逗号(中英文都可以)为分割，空格将被自动过滤"
+    style="height:100%;width:100%;" v-model="nameListRaw">
+  </textarea>
+  </div>
   <template v-if="isRes">
     <div class="panel panel-success" style="margin-top: 3rem;">
       <div class="panel-heading">res</div>
       <div class="panel-body"><p>{{message}}</p></div>
-      <table class="table" style="margin:0.5rem;">
-        <tr><th>UserID</th><th>Username</th><th>Password</th></tr>
-	<tbody>
-        <tr v-for="(user, index) in resMsg" :key="index">
-          <td>{{user.user_id}}</td>
-          <td>{{user.username}}</td>
-          <td>{{user.password}}</td>
+      <table v-if="resMsg" class="table" style="margin:0.5rem;">
+        <tr>
+          <th>UserID</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Password</th>
+          <th>Dummy Email</th>
         </tr>
-	</tbody>
+        <tbody>
+          <tr v-for="(user, index) in resMsg" :key="index">
+            <td>{{user.user_id}}</td>
+            <td>{{user.name}}</td>
+            <td>{{user.username}}</td>
+            <td>{{user.password}}</td>
+            <td>{{user.email}}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   </template>
@@ -36,7 +49,10 @@ export default {
       coldTime: 0,
       contestID: 1001,
       accountNum: 0,
-      isRes: false
+      nameListRaw: '',
+      isRes: false,
+      message: '',
+      resMsg: []
     }
   },
   methods: {
@@ -59,22 +75,39 @@ export default {
       let vm = this
       if (vm.accountNum === 0) return
       vm.setColdTime()
-      vm.$http.get(window.noPointHost + '/api/admin/user/addmulti/' + vm.contestID + '/' + vm.accountNum)
-        .then(function (res) {
+      let nameList = vm.nameListRaw.replace(' ', '').split(/,|，/)
+      console.log(nameList)
+      if (nameList.length !== parseInt(vm.accountNum) || vm.nameListRaw === '') {
+        vm.message = `Error: Add ${vm.accountNum} users, but ${nameList.length} names are given`
+        vm.isRes = true
+        return
+      }
+      let sendPackage = new FormData()
+      sendPackage.append('nameList', nameList)
+      sendPackage.append('cid', parseInt(vm.contestID))
+      sendPackage.append('num', parseInt(vm.accountNum))
+      vm.$http.post(window.noPointHost + '/api/admin/user/addmulti/', {nameList: nameList, cid: parseInt(vm.contestID), num: parseInt(vm.accountNum)}).then(
+        res => {
           console.log(JSON.stringify(res))
-          vm.isRes = true
           vm.message = `code: ${res.body.code}, message: ${res.body.message} ` + JSON.stringify(res.body.error)
           let resMsgList = []
           const resList = res.body.data.resUser
           for (let i = 0, len = resList.length; i < len; i++) {
             resMsgList.push({
               user_id: resList[i].user_id,
+              name: resList[i].name,
               username: resList[i].username,
-              password: resList[i].password
+              password: resList[i].password,
+              email: resList[i].email
             })
           }
           vm.resMsg = resMsgList
-        })
+        },
+        err => {
+          vm.message = JSON.stringify(err)
+          console.error(err)
+        }
+      )
       vm.isRes = true
     }
   }
