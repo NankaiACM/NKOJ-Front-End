f842788ea7cdb073cf573336826a01d11afe2623
Zhihao Lyu <zlyu@mail.nankai.edu.cn>
2021-10-03 11:24:52 -0500

    ts compatibility for ie11

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/app', { useBuiltIns: 'entry' }]
+    // '@vue/cli-plugin-babel/preset'
   ]
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'core-js/stable'
+import '@/plugins/vuetify'
 
 const app = new Vue({
   router,
